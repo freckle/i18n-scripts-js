b485366f1969e444b05dbe7ec436cfcd9a515ec5
✿ corey (they/them) <corey@x64.co>
2023-03-20 15:38:54 -0700

    Wrappers to execute Bash scripts

---
+++ src/check-i18next-icu-parser.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const {spawn} = require('node:child_process')
+const path = require('path')
+
+const [_nodeBin, scriptPath, ...args] = process.argv
+
+const proc = spawn(`${path.dirname(scriptPath)}/check-i18next-icu-parser`, args)
+
+proc.stdout.on('data', data => {
+  process.stdout.write(data)
+})
+
+proc.stderr.on('data', data => {
+  process.stderr.write(data)
+})
